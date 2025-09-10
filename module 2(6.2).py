1e0dfd8a75102db6b6683d41d06cdf1dcbdc1d8f
Miheret Berhe <158329965+miheretberhe@users.noreply.github.com>
2025-09-09 21:06:06 -0400

    Add files via upload

---
+++ module 2(6.2).py
@@ -0,0 +1,16 @@
+#Miheret Gebrehans
+#Assignment 6.1 
+#08 September 2025
+
+guess_me = 7
+number = 1
+
+while True:
+    if number < guess_me:
+        print("too low")
+    elif number == guess_me:
+        print("found it!")
+        break
+    else:
+        print("oops")
+        breaknumber +=1
