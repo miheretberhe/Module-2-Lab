4631ba536982300865c25e02bfd8830173b2dbbd
Miheret Berhe <158329965+miheretberhe@users.noreply.github.com>
2025-09-09 20:50:39 -0400

    Add files via upload

---
+++ module 2(4.2).py
@@ -0,0 +1,16 @@
+#Miheret Gebrehans
+#Assignment 4.2
+#This code distinguishs different fruits by size and color.
+# 09 September 2025
+
+small = False
+green = True
+
+if small and green:
+    print("pea")
+elif small and not green:
+    print("cherry")
+elif not small and green:
+    print("watermelon")
+elif not small and not green:
+    print("pumpkin")
